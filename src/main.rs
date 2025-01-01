5cc24730c26868ccad155d009bed0dceb5dbd9c0
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-01-01 15:51:08 +0300

    implement basic configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(addr_parse_ascii)]
+mod config;
+
+use config::load_config;
+
+fn main() -> anyhow::Result<()> {
+    let config = load_config(std::env::args().nth(1).expect("no config file provided"))?;
+    Ok(())
 }
