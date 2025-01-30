ea3fcacbae1efc7e961748032929d480eff70291
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-01-30 18:29:05 +0300

    prevent local packets from exiting

--- src/routing.rs
+++ src/routing.rs
@@ -61,9 +61,11 @@ impl Router {
     }
 
     pub async fn route_packet(&self, packet: Box<[u8]>) -> anyhow::Result<()> {
-        if let RoutingResult::Error(err) = self.route_local(&packet).await {
-            return Err(err);
-        }
+        match self.route_local(&packet).await {
+            RoutingResult::Error(err) => return Err(err),
+            RoutingResult::Ok => return Ok(()),
+            _ => {}
+        };
 
         let mut lock = self.tun_writer.lock().await;
         let mut offset = 0;
