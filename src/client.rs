bcc5433c633b779785287cc5f78953b7659320bf
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-02-03 14:18:33 +0300

    add stop token to client

--- src/client.rs
+++ src/client.rs
@@ -12,6 +12,7 @@ use std::{
 use tokio::{
     io::{AsyncRead, AsyncReadExt, AsyncWrite, ReadHalf, WriteHalf},
     net::TcpStream,
+    sync::watch,
 };
 use tokio_rustls::{rustls, TlsConnector};
 use tun::{AbstractDevice, AsyncDevice};
@@ -19,14 +20,23 @@ use tun::{AbstractDevice, AsyncDevice};
 pub struct Client {
     connector: TlsConnector,
     socket_address: SocketAddr,
+    stop_sender: watch::Sender<bool>,
+    stop_receiver: watch::Receiver<bool>,
 }
 
 impl Client {
     pub fn try_new(config: ClientConfig, tls: TlsConfig) -> anyhow::Result<Self> {
+        let (sender, receiver) = watch::channel(false);
         Ok(Self {
             connector: Arc::new(configure_tls(tls)?).into(),
             socket_address: config.address,
+            stop_sender: sender,
+            stop_receiver: receiver,
         })
+    }
+
+    pub fn stop_sender(&self) -> watch::Sender<bool> {
+        self.stop_sender.clone()
     }
 
     pub async fn run(self) -> anyhow::Result<()> {
@@ -44,8 +54,8 @@ impl Client {
         let mtu = device.mtu().unwrap() as usize;
         let (tun_reader, tun_writer) = tokio::io::split(device);
 
-        let send_fut = send_tun(packet_receiver, tun_writer);
-        let receive_fut = receive_tun(packet_sender, tun_reader, mtu);
+        let send_fut = send_tun(packet_receiver, tun_writer, self.stop_receiver.clone());
+        let receive_fut = receive_tun(packet_sender, tun_reader, mtu, self.stop_receiver.clone());
         let (res_send, res_receive) = tokio::join!(send_fut, receive_fut);
         if let Err(e) = res_send {
             error!("send error: {}", e);
@@ -94,25 +104,53 @@ async fn configure_tun<IO: AsyncRead + Unpin>(
 async fn send_tun<IO: AsyncRead + Unpin>(
     mut receiver: PacketReceiver<IO>,
     mut tun: WriteHalf<AsyncDevice>,
+    mut stop_receiver: watch::Receiver<bool>,
 ) -> anyhow::Result<()> {
-    loop {
-        let packet = receiver.receive().await?;
-        full_send(&mut tun, &packet).await?;
+    while !*stop_receiver.borrow_and_update() {
+        let stop_fut = stop_receiver.changed();
+        let packet_fut = receiver.receive();
+        tokio::select! {
+            res = stop_fut => {
+                if res.is_err() {
+                    break;
+                }
+                continue;
+            }
+            packet_res = packet_fut => {
+                let packet = packet_res?;
+                full_send(&mut tun, &packet).await?;
+            }
+        }
     }
+    Ok(())
 }
 
 async fn receive_tun<IO: AsyncWrite + Unpin>(
     mut sender: PacketSender<IO>,
     mut tun: ReadHalf<AsyncDevice>,
     mtu: usize,
+    mut stop_receiver: watch::Receiver<bool>,
 ) -> anyhow::Result<()> {
     let mut buf = vec![0u8; mtu];
-    loop {
-        let received = tun.read(buf.as_mut_slice()).await?;
-        // maybe not?
-        if received == 0 {
-            return Ok(());
+    while !*stop_receiver.borrow_and_update() {
+        let stop_fut = stop_receiver.changed();
+        let read_fut = tun.read(buf.as_mut_slice());
+        tokio::select! {
+            res = stop_fut => {
+                if res.is_err() {
+                    break;
+                }
+                continue;
+            }
+            packet_res = read_fut => {
+                let received = packet_res?;
+                // maybe not?
+                if received == 0 {
+                    return Ok(());
+                }
+                sender.send(&buf[..received]).await?;
+            }
         }
-        sender.send(&buf[..received]).await?;
     }
+    Ok(())
 }
